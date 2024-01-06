6b3c518037fc0782f44052e5349a55216fce9d7c
kzares-dev <138066850+kzares-dev@users.noreply.github.com>
2024-01-05 23:40:36 -0500

    fixed webhook

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,21 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    if(cached.conn) return cached.conn;
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    if(!MONGODB_URI) throw new Error("MONGODB_URI IS MISSING");
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    });
+  cached.conn = await cached.promise;
 
-    cached.conn = await cached.promise;
-
-    return cached.conn
+  return cached.conn;
 }
